7d9d28e8996c5043e107a204f37af98239514e27
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-03-29 12:38:32 +0530

    basic threejs setup

---
+++ shaders/fragment.glsl
@@ -0,0 +1,10 @@
+uniform float time;
+uniform float progress;
+uniform sampler2D landscape;
+varying vec2 vUv;
+varying vec3 vPosition;
+float PI = 3.141592653589793238;
+
+void main() {
+  gl_FragColor = vec4(vUv, 0.0, 1.0);
+}
